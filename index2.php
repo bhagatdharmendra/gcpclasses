bf877bfe02189a47dfd08b04e68c269508aa596a
dharmendra bhagat <51524315+bhagatdharmendra@users.noreply.github.com>
2020-09-13 17:13:02 +0530

    index2.php
    for index2.php

---
+++ index2.php
@@ -0,0 +1,12 @@
+##### code by dharmendra bhagat#####
+
+<h1> <center> welcome everyone...!! </center> </h1> <br>
+<body style="background-color:#FCCB83;">
+<br />
+
+<pre>
+<?php
+print `ifconfig`;
+
+?>
+</pre>
