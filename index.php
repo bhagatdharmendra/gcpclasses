fa6ee60ee64ce14d7816c8eea5f96075b7c20397
root <root@gcpclass-git.asia-southeast1-b.c.developer-111120.internal>
2020-09-13 11:16:29 +0000

    first commit

---
+++ index.php
@@ -0,0 +1,12 @@
+##### code by dharmendra bhagat#####
+
+<h1> <center> welcome everyone...!! </center> </h1> <br>
+<br />
+
+<pre>
+<?php
+body {background-color: #92a8d1;}
+print `ifconfig`;
+
+?>
+</pre>
