becf9fb686b89390f9fa487be4e09ef459b7a1fd
dharmendra bhagat <51524315+bhagatdharmendra@users.noreply.github.com>
2021-06-23 14:46:53 +0530

    Update index2.php

--- index2.php
+++ index2.php
@@ -1,7 +1,7 @@
 ##### code by dharmendra bhagat#####
 
 <h1> <center> welcome everyone...!! </center> </h1> <br>
-<body style="background-color:#FF4500;">
+<body style="background-color:#1affa3;">
 <br />
 
 <pre>
